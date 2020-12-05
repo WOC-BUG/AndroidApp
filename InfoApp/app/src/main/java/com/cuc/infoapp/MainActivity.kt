ea1a55f7a0dc33087ac5c8d2e05791c6664c781f
WOC_BUG <1539777243@qq.com>
2020-12-05 14:21:11 +0800

    添加按钮按下样式

--- InfoApp/app/src/main/java/com/cuc/infoapp/MainActivity.kt
+++ InfoApp/app/src/main/java/com/cuc/infoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.cuc.infoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun search(view: View) {}
 }
