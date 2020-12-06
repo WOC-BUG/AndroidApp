f96d8484e775b1e30462fdb79fba835699f8df06
WOC_BUG <1539777243@qq.com>
2020-12-06 18:24:42 +0800

    项目分层

--- InfoApp/app/src/main/java/com/cuc/infoapp/view/Activity/MainActivity.kt
+++ InfoApp/app/src/main/java/com/cuc/infoapp/view/Activity/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.cuc.infoapp
+package com.cuc.infoapp.view.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.cuc.infoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
