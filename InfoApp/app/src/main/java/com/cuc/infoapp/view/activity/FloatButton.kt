78d56b5b0fca2fc90a66a0f08027dead10563470
Jijialu <1258015215@qq.com>
2020-12-07 01:03:41 +0800

    增加悬浮按钮

---
+++ InfoApp/app/src/main/java/com/cuc/infoapp/view/activity/FloatButton.kt
@@ -0,0 +1,26 @@
+package com.cuc.infoapp.view.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.cuc.infoapp.R
+import com.github.clans.fab.FloatingActionButton
+import kotlinx.android.synthetic.main.item_news.*
+
+
+class FloatButton : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.home_main)
+        var buttonrobot:ImageView=view.findViewById(R.id.ButtonRobot)
+    }
+//    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+//        public void onClick(View:view) {
+//            //TODO
+//
+//        }
+//    })
+//}
