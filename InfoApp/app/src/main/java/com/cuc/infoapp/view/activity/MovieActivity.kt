6972e5a3c38c65d8d4cd0b972abd25fb444bb27b
Lynn <2522937408@qq.com>
2020-12-08 00:21:07 +0800

    添加movie评论功能

--- InfoApp/app/src/main/java/com/cuc/infoapp/view/activity/MovieActivity.kt
+++ InfoApp/app/src/main/java/com/cuc/infoapp/view/activity/MovieActivity.kt
@@ -2,17 +2,22 @@ package com.cuc.infoapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cuc.infoapp.R
 import com.cuc.infoapp.pojo.Comment
 import com.cuc.infoapp.view.adapter.CommentAdapter
 import com.cuc.infoapp.view.adapter.PerformerAdapter
+import kotlinx.android.synthetic.main.chattingrobot_main.*
 import kotlinx.android.synthetic.main.movie_content.*
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() , View.OnClickListener{
 
     private val actorList = ArrayList<String>()    //演员列表
     private val commentList = ArrayList<Comment>()  //评论列表
+
+    private var actorAdapter : PerformerAdapter ?= null
+    private var commentAdapter : CommentAdapter ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +27,16 @@ class MovieActivity : AppCompatActivity() {
         val layoutManager1 = LinearLayoutManager(this)
         layoutManager1.orientation = LinearLayoutManager.HORIZONTAL  //横向展示items
         performerRecyclerView.layoutManager = layoutManager1
-        val actorAdapter = PerformerAdapter(actorList)
+        actorAdapter = PerformerAdapter(actorList)
         performerRecyclerView.adapter = actorAdapter
 
         this.initComments()
         val layoutManager2 = LinearLayoutManager(this)
         commentRecyclerView.layoutManager = layoutManager2
         layoutManager2.orientation= LinearLayoutManager.VERTICAL     //纵向展示items
-        val commentAdapter = CommentAdapter(commentList)
+        commentAdapter = CommentAdapter(commentList)
         commentRecyclerView.adapter = commentAdapter
+        accept_comment.setOnClickListener(this)
     }
 
     //初始化演员数组
@@ -51,4 +57,32 @@ class MovieActivity : AppCompatActivity() {
         commentList.add(Comment("Charles", R.drawable.user3, "I like it!"))
 
     }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            accept_comment -> {
+                val content = comment_box.text.toString()
+                if(content.isNotEmpty()){
+                    val i = (1..3).random()
+                    val cmn = Comment(getRandomName(),R.drawable.user2,content)
+                    commentList.add(cmn)
+                    //当有新评论，刷新RecyclerView中的显示
+                    commentAdapter?.notifyItemInserted(commentList.size-1)
+                    //将RecyclerView定位到最后一行
+                    commentRecyclerView.scrollToPosition(commentList.size-1)
+                    comment_box.setText("")  //清空输入框
+                }
+            }
+        }
+    }
+    //产生随机字符串
+    private fun getRandomName():String{
+        var uName : String = ""
+        val list = mutableListOf<String>("dshXA01","hjdk_9","kid03","abc8","gds008","hser_3")
+        list.shuffled().take(1).forEach{
+            uName = it.toString()
+        }
+        return uName
+    }
+
 }
