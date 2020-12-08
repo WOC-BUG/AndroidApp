b002b589e983544a38637cd08e7dc07dec96ac2c
Jijialu <1258015215@qq.com>
2020-12-08 12:15:11 +0800

    Fragment调试+1

--- InfoApp/app/src/main/java/com/cuc/infoapp/view/activity/MainActivity.kt
+++ InfoApp/app/src/main/java/com/cuc/infoapp/view/activity/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.movie_content)
+       // setContentView(R.layout.movie_content)
         //listFragment.add(MovieContentFragment())    //添加电影详情Fragment
 
         //初始化控件
